fe42158e1a7717234364716259ac88bf9d085a03
EelkeFeitz <eelkefeitz@live.nl>
2017-05-28 11:53:04 +0200

    + WEEK3-1

---
+++ week3-1/wall.hpp
@@ -0,0 +1,19 @@
+#ifndef WALL_HPP
+#define WALL_HPP
+
+#include "window.hpp"
+#include "rectangle.hpp"
+#include "vector.hpp"
+
+class wall : public rectangle {
+private:
+    vector top_left;
+    vector bot_right;
+    int updateCounter = 0;
+public:
+    int updateFrequency;
+    wall( window & w, const vector & top_left , const vector & bot_right, const bool filled, const int updateFrequency );
+    void update() override;
+};
+
+#endif // WALL_HPP
